cd8cb0fe544734622df5b03fd8980f3488a84871
Shaashwat Sharma <shaashwatsharma@Shaashwats-Macbook-Pro.local>
2025-06-30 13:23:44 +0530

    init

---
+++ notification-service/src/main.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log("notification-service service running...");
+  // Example usage
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
